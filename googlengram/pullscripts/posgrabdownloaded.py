b0c1d43b50a8f92ca4f8db83fd4127379935ac4d
Alaa ElEbshihy <alaaebshihy.git@gmail.com>
2017-12-26 19:13:52 +0200

    Updated the requirements and posgrab Added installations steps for ubuntu.

--- googlengram/pullscripts/posgrabdownloaded.py
+++ googlengram/pullscripts/posgrabdownloaded.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import argparse
 import collections
+import shutil
 
 import ioutils
 
@@ -10,21 +11,61 @@ VERSION = '20120701'
 TYPE = '1gram'
 POS = re.compile('[^_]+_[A-Z]+')
 
-def main(in_dir, out_dir, years):
-    year_counts = {}
-    year_doc_counts = {}
-    year_pos = {}
+def remove_tmp_dirs(out_dir, name_list):
+    for name in name_list:
+        try:
+            shutil.rmtree(out_dir + "/" + name)
+        except:
+            pass 
+
+def merge_year_counts(out_dir, name_list, years):
     for year in years:
-        year_pos[year] = {}
-        year_counts[year] = {}
-        year_doc_counts[year] = {}
+        year_counts = {}
+        year_doc_counts = {}
+        year_pos = {}
+
+        for name in name_list:
+            tmp_year_counts = ioutils.load_pickle(out_dir + "/" + name + "/" + str(year) + "-counts.pkl")
+            tmp_year_doc_counts = ioutils.load_pickle(out_dir + "/" + name + "/" + str(year) + "-doc_counts.pkl")
+            tmp_year_pos = ioutils.load_pickle(out_dir + "/" + name + "/" + str(year) + "-pos.pkl")
+            for word, count in tmp_year_counts.iteritems():
+                if not word in year_counts:
+                    year_counts[word] = 0
+                    year_doc_counts[word] = 0
+                    year_pos[word] = collections.Counter()
+                year_counts[word] += tmp_year_counts[word]
+                year_doc_counts[word] += tmp_year_doc_counts[word]
+                counter_keys = tmp_year_pos[word].keys()
+                for pos in counter_keys:
+                    year_pos[word][pos] += tmp_year_pos[word][pos]
+
+        print "Writing merged counts for " + str(year) + " ..."
+        ioutils.write_pickle(year_counts, out_dir + str(year) + "-counts.pkl")
+        ioutils.write_pickle(year_doc_counts, out_dir + str(year) + "-doc_counts.pkl")
+        ioutils.write_pickle(year_pos, out_dir + str(year) + "-pos.pkl")
+
+    print "Deleting temp dirs ..."
+    remove_tmp_dirs(out_dir, name_list)
+
+
+def main(in_dir, out_dir, years):
+    name_list = []
 
     print "Start loop"
     for zipped_file in os.listdir(in_dir):
         if not os.path.isfile(in_dir + "/" + zipped_file) or not zipped_file.endswith((".gz")):
             continue
 
+        tmp_year_counts = {}
+        tmp_year_doc_counts = {}
+        tmp_year_pos = {}
+        for year in years:
+            tmp_year_counts[year] = {}
+            tmp_year_doc_counts[year] = {}
+            tmp_year_pos[year] = {}
+
         name = zipped_file.split(".gz")[0]
+        name_list.append(name)
         print  "Unzipping", name
         subprocess.call(['gunzip', '-f', in_dir + "/" + name + '.gz', '-d'])
 
@@ -47,16 +88,23 @@ def main(in_dir, out_dir, years):
                     doc_count = int(split[3])
                     if not year in years:
                         continue
-                    if not word in year_counts[year]:
-                        year_counts[year][word] = 0
-                        year_doc_counts[year][word] = 0
-                        year_pos[year][word] = collections.Counter() 
-                    year_counts[year][word] += count 
-                    year_doc_counts[year][word] += doc_count 
-                    year_pos[year][word][pos] += count
+                    if not word in tmp_year_counts[year]:
+                        tmp_year_counts[year][word] = 0
+                        tmp_year_doc_counts[year][word] = 0
+                        tmp_year_pos[year][word] = collections.Counter() 
+                    tmp_year_counts[year][word] += count 
+                    tmp_year_doc_counts[year][word] += doc_count 
+                    tmp_year_pos[year][word][pos] += count
                 except UnicodeDecodeError:
                      pass
 
+        print "Writing tmp " + name
+        ioutils.mkdir(out_dir + "/" + name)
+        for year in years:
+            ioutils.write_pickle(tmp_year_counts[year], out_dir + "/" + name + "/" + str(year) + "-counts.pkl")
+            ioutils.write_pickle(tmp_year_doc_counts[year], out_dir + "/" + name + "/" + str(year) +  "-doc_counts.pkl")
+            ioutils.write_pickle(tmp_year_pos[year], out_dir + "/" + name + "/" + str(year) +  "-pos.pkl")
+        
         print "Deleting", name
         try:
             os.remove(in_dir + "/" + name)
@@ -64,11 +112,9 @@ def main(in_dir, out_dir, years):
         except:
             pass
 
-    print "Writing..."
-    for year in years:
-        ioutils.write_pickle(year_counts[year], out_dir + str(year) + "-counts.pkl")
-        ioutils.write_pickle(year_doc_counts[year], out_dir + str(year) + "-doc_counts.pkl")
-        ioutils.write_pickle(year_pos[year], out_dir + str(year) + "-pos.pkl")
+    print "Merging..."
+    merge_year_counts(out_dir, name_list, years)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Pulls and saves unigram data.")
---
+++ install_ubuntu.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install python
+sudo apt-get install python-setuptools
+sudo apt-get install python-pip
+export LC_ALL=C
+sudo pip install --no-cache-dir -r requirements.txt
+sudo pip install Cython
+python setup.py build_ext --inplace
