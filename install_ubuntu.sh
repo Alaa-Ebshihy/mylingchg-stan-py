445f53f8050f32a749c5aac01ccbc10aac166f46
Alaa ElEbshihy <alaaebshihy.git@gmail.com>
2017-12-27 17:15:53 +0200

    Updated install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -5,4 +5,4 @@ sudo apt-get install python-pip
 export LC_ALL=C
 sudo pip install --no-cache-dir -r requirements.txt
 sudo pip install Cython
-python setup.py build_ext --inplace
+sudo python setup.py build_ext --inplace
