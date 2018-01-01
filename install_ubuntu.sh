c3888ec7c79c8f17f8d7b9767a3a3627cf115515
Alaa ElEbshihy <alaaebshihy.git@gmail.com>
2018-01-01 11:37:33 +0200

    Updated installubuntu.sh.

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,8 +1,22 @@
 sudo apt-get update
+
 sudo apt-get install python
 sudo apt-get install python-setuptools
+
+#install and upgrade pip
 sudo apt-get install python-pip
-export LC_ALL=C
+pip install --upgrade pip
+echo 'export LC_ALL=C' >> ~/.bashrc
+. ~/.bashrc
+
+#install requirements
 sudo pip install --no-cache-dir -r requirements.txt
 sudo pip install Cython
+
+#build cython modules
 sudo python setup.py build_ext --inplace
+
+##update PYTHONPATH env variable
+echo 'export PYTHONPATH=/implementation/mylingchg-stan-py' >> ~/.bashrc
+echo 'export PYTHONPATH=$PYTHONPATH:/implementation/mylingchg-stan-py/googlengram' >> ~/.bashrc
+. ~/.bashrc
