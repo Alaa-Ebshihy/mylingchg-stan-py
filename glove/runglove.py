438e52a9f4eb3723f938d6fec3007d1777f1626b
Alaa ElEbshihy <alaaebshihy.git@gmail.com>
2018-01-27 23:33:57 +0200

    Added learning rate as args in glove.

--- glove/runglove.py
+++ glove/runglove.py
@@ -7,7 +7,7 @@ VOCAB_FILE = "{year:d}.vocab"
 INPUT_FILE = "{year:d}-pair_counts.shuf.bin"
 SAVE_FILE = "{year:d}"
 
-def train_years(years, vocab_dir, count_dir, out_dir, dim, iter, workers, alpha, x_max):
+def train_years(years, vocab_dir, count_dir, out_dir, dim, iter, workers, alpha, x_max, eta):
     for i, year in enumerate(years):
         print "Running year", year
         subprocess.call(['./glovef/build/glove',
@@ -19,6 +19,7 @@ def train_years(years, vocab_dir, count_dir, out_dir, dim, iter, workers, alpha,
                 '-iter', str(iter),
                 '-alpha', str(alpha),
                 '-x-max', str(x_max),
+                '-eta', str(eta),
                 '-vocab-file', vocab_dir + VOCAB_FILE.format(year=year),
                 '-words-file', vocab_dir + VOCAB_FILE.format(year=year),
                 '-contexts-file', vocab_dir + VOCAB_FILE.format(year=year),
@@ -34,6 +35,7 @@ if __name__ == "__main__":
     parser.add_argument("--workers", type=int, default=50)
     parser.add_argument("--alpha", type=float, default=0.75)
     parser.add_argument("--x-max", type=float, default=100.0)
+    parser.add_argument("--eta", type=float, default=0.05)
     parser.add_argument("--start-year", type=int, default=1800)
     parser.add_argument("--end-year", type=int, default=2000)
     parser.add_argument("--year-inc", type=int, default=1)
@@ -41,5 +43,5 @@ if __name__ == "__main__":
     out_dir = args.out_dir + "/" + str(args.dim) + "/"
     mkdir(out_dir)
     years = range(args.start_year, args.end_year + 1, args.year_inc)
-    train_years(years, args.vocab_dir + "/", args.count_dir + "/", out_dir, args.dim, args.iter, args.workers, args.alpha, args.x_max)
+    train_years(years, args.vocab_dir + "/", args.count_dir + "/", out_dir, args.dim, args.iter, args.workers, args.alpha, args.x_max, args.eta)
 
