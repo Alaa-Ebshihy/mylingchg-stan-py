a1dfe44c3b96447343e09175f67562bf918ac240
Alaa ElEbshihy <alaaebshihy.git@gmail.com>
2018-03-29 12:55:27 +0200

    Added extension to word vectors in detect_cp.sh

--- timeseriesanalysis/detect_cp_distributional.sh
+++ timeseriesanalysis/detect_cp_distributional.sh
@@ -6,11 +6,12 @@ STARTTIMEPOINT=$4
 ENDTIMEPOINT=$5
 STEP=$6
 MODEL_FAMILY=$7
-KNN=$8
-FILTER_VOCAB_FILE=${9}
-BOOTSTRAP=${10}
-THRESHOLD=${11}
-WORKERS=${12}
+MODEL_EXT=$8
+KNN=$9
+FILTER_VOCAB_FILE=${10}
+BOOTSTRAP=${11}
+THRESHOLD=${12}
+WORKERS=${13}
 
 EMBEDDINGS_TYPE=skipgram
 echo "Output directory is", $OUTPUT_DIR
@@ -21,7 +22,7 @@ mkdir -p $OUTPUT_DIR
 echo "Mapping to joint space"
 mkdir -p $WORKING_DIR/predictors
 echo "Predictors will be stored in", $WORKING_DIR/predictors
-arr=("$INPUT_DIR/*.model")
+arr=("$INPUT_DIR/*.$MODEL_EXT")
 ((FINALTIMEPOINT=$ENDTIMEPOINT-$STEP))
 parallel -j${WORKERS} python learn_map.py -k ${KNN} -f $WORKING_DIR/predictors/{/.}.predictor -o {} -n {//}/${FINALTIMEPOINT}_*.model -m $MODEL_FAMILY ::: $arr
 
@@ -39,6 +40,6 @@ echo "Creating time series"
 mkdir -p $WORKING_DIR/timeseries/
 python tsconstruction/dump_timeseries.py -f $WORKING_DIR/displacements/timeseries_s_t_words.pkl -s $WORKING_DIR/timeseries/source.csv -e $WORKING_DIR/timeseries/dest.csv -m $STARTTIMEPOINT -n $ENDTIMEPOINT -st $STEP -me "polar" -metric "cosine" -workers ${WORKERS}
 
-python detect_changepoints_word_ts.py -f $WORKING_DIR/timeseries/source.csv -v $FILTER_VOCAB_FILE -p $OUTPUT_DIR/pvals.csv -n $OUTPUT_DIR/samples.csv -c $STARTTIMEPOINT -w ${WORKERS} -b ${BOOTSTRAP} -t ${THRESHOLD}
+python detect_changepoints_word_ts.py -f $WORKING_DIR/timeseries/source.csv -v $FILTER_VOCAB_FILE -p $OUTPUT_DIR/pvals_ts.csv -n $OUTPUT_DIR/samples_ts.csv -c $STARTTIMEPOINT -w ${WORKERS} -b ${BOOTSTRAP} -t ${THRESHOLD}
 
-python demonstrate_cp.py -f $WORKING_DIR/timeseries/source.csv -p $OUTPUT_DIR/pvals_1.csv -n $OUTPUT_DIR/samples_1.csv -c $STARTTIMEPOINT -w ${WORKERS} -b ${BOOTSTRAP} -t ${THRESHOLD}
+python demonstrate_cp.py -f $WORKING_DIR/timeseries/source.csv -p $OUTPUT_DIR/pvals_demon.csv -n $OUTPUT_DIR/samples_demon.csv -c $STARTTIMEPOINT -w ${WORKERS} -b ${BOOTSTRAP} -t ${THRESHOLD}
